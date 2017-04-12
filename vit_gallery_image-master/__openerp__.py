e3a88f35ba5ca91c95cb3d713209b52f97ded0c7
abangbule <gian.ule@gmail.com>
2017-04-13 06:42:22 +0700

    my addons odoo

---
+++ vit_gallery_image-master/__openerp__.py
@@ -0,0 +1,26 @@
+{
+	"name": "Galery Image", 
+	"version": "1.0", 
+	"depends": [
+		"base",
+		"board"
+	], 
+	"author": "ryanlinnok@gmail.com", 
+	"category": "galery", 
+	"description": """\
+
+Manage
+======================================================================
+
+* Menambahkan galery image
+
+""",
+	"data": [
+		"menu.xml", 
+		"view/galery.xml",
+		"view/galery_image.xml"
+	],
+	"installable": True,
+	"auto_install": False,
+	"application": True,
+}
